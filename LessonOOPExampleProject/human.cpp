d9941648c24b9eb0e4fd4b2f0224b77b39f81fea
Maria Tarasenko <taracamary@gmail.com>
2023-05-16 18:49:39 +0300

    Use inheritan Add base Human class and subclasses: Student, Director, Teacher, Worker добавление базового и производных классов

---
+++ LessonOOPExampleProject/human.cpp
@@ -0,0 +1,52 @@
+#include "human.h"
+
+// default constructor
+Human::Human() {
+	name = "no name";
+	surname = "no surname";
+	age = 15;
+}
+
+// constructor with arguments
+Human::Human(string nm, string surnm, int a) {
+	name = nm;
+	surname = surnm;
+	age = a;
+}
+
+Human::~Human() { }
+
+string Human::getName() {
+	return name;
+}
+
+void Human::setName(string n) {
+	name = n;
+}
+
+string Human::getSurname() {
+	return surname;
+}
+
+void Human::setSurname(string n) {
+	surname = n;
+}
+
+int Human::getAge() {
+	return age;
+}
+
+void Human::setAge(int a) {
+	if (a > 0 && a < 110) {
+		age = a;
+	}
+}
+
+string Human::convert() {
+	string msg = "";
+	msg += name;
+	msg += " " + surname;
+	msg += " ( age = " + to_string(age);
+	msg += ")";
+	return msg;
+}
---
+++ LessonOOPExampleProject/human.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "main.h"
+
+class Human
+{
+private:
+	string name;
+	string surname;
+	int age;
+	
+public:
+	Human();
+	Human(string nm, string surnm, int a);
+	~Human();
+
+	string getName();
+	void setName(string n);
+	string getSurname();
+	void setSurname(string n);
+	int getAge();
+	void setAge(int a);
+	
+	string convert();
+};
+
